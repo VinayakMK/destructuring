ea7deef1c6ccbd45febca54cce57ad8758b9c67c
Vinayak MK <48713290+VinayakMK@users.noreply.github.com>
2023-09-20 23:59:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+/*let arr = ["Alex",23,"London"];
+//let [userName,age,place] = arr;
+let [userName,,place] = arr;
+console.log(userName,place);*/
+
+
+/*let session = {
+    topic : "JavaScript",
+    time : 9,
+    strength : 50
+}
+
+let {topic, time, strength} = session;
+console.log(session);*/
+
+
+/*let plteams = ["Arsenal",1,"Arteta"];
+let [teamName,position,coach] = plteams;
+console.log(teamName,position);*/
+
+let show = {
+    name : "The Office",
+    time : "9 PM",
+    rating : 4.5
+}
+
+let {name,time,rating} = show;
+console.log(show);
